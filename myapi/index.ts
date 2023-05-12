ca744a06effa2c30303482ca0b1f3d10916f164c
salvatore <salvatore.sorrentino@live.com>
2023-05-12 10:02:14 +0200

    concurrently

--- myapi/index.ts
+++ myapi/index.ts
@@ -3,12 +3,14 @@ import { User } from './models/user';
 const app = express();
 
 app.get('/', (request, response) => {
-    response.send("Ciaooooo Ciaooooo  dalla mia API");
+    response.send("Ciao Ciaoooo  dalla mia API");
 });
 
 app.get("/users", (request,response) => {
-    const users: User[] = [ { id: 1, name: "Mario", surname: "Rossi"}  ];
-    
+    const users: User[] = [ 
+        { id: 1, name: "Mario", surname: "Rossi"},
+        { id: 2, name: "Luigi", surname: "Bianchi"},
+    ];
     response.json(users);
 });
 
